4b89d0d3175ef1e363504be446de9553e0834217
DalaScript <nik.dalalishvili@gmail.com>
2025-05-11 18:01:29 +0400

    added fetch api

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log('testing');
+// Unsplash API
+const count = 10;
+const apiKey = 'j8wXINJMk2o0JhukcJYrpXmbFV4GdVXwwnSlaVeS228';
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+// Get photos from Unsplash API
+async function getPhotos() {
+    try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        console.log(data);
+    }catch (error) {
+        // Catch Error Here
+    }
+}
+
+// On Load
+getPhotos();
